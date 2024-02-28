f62133a775501fba723148c923c9c5c7922f4f7a
Raul Jr <rauljraraujo2090@gmail.com>
2024-02-28 11:58:24 -0500

    fff

---
+++ include/footer.php
@@ -0,0 +1,153 @@
+<footer class=footer>
+            <div class=container>
+                <div class="footer_wrapper d-sm-flex flex-wrap flex-lg-nowrap justify-content-lg-between">
+                    <div class="footer_block col-sm-6 col-lg-auto" data-order=1>
+                        <div class="logo logo--footer">
+                            <a class="d-inline-flex align-items-center" href=index.html>
+                                <span class=logo_picture>
+                                    <img src=svg/logo.svg alt=Edison>
+                                </span>
+                                <span class=text>
+                                    <span class=brand>Peru</span>
+                                    <span class=text_secondary>Suizo</span>
+                                </span>
+                            </a>
+                        </div>
+                        <p class=footer_block-text>Curabitur non libero at lorem finibus lobortis. Ut auctor egestas pretium. Proin nunc ligula, venenatis tempor</p>
+                        <ul class="footer_block-socials d-flex align-items-center">
+                            <li class=footer_block-socials_item>
+                                <a class=link href=https://www.facebook.com target=_blank rel="noopener noreferrer">
+                                    <i class=icon-facebook></i>
+                                </a>
+                            </li>
+                            <li class=footer_block-socials_item>
+                                <a class=link href=https://www.twitter.com target=_blank rel="noopener noreferrer">
+                                    <i class=icon-twitter></i>
+                                </a>
+                            </li>
+                            <li class=footer_block-socials_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <i class=icon-instagram></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="wrapper d-flex flex-column">
+                            <a class="link link--contacts text text--sm d-inline-flex align-items-center" href=mailto:example@domain.com>
+                                <i class="icon-envelope icon"></i>
+                                peru suizo@mail.com 
+                            </a>
+                            <a class="link link--contacts text text--sm d-inline-flex align-items-center" href=tel:+123456789>
+                                <i class="icon-phone-solid icon"></i>
+                                +51 904 240 525
+                            </a>
+                        </div>
+                    </div>
+                    <div class="footer_block col-sm-6 col-lg-auto" data-order=2>
+                        <h5 class=footer_block-header>Información:</h5>
+                        <ul class=footer_block-nav>
+                            <li class=footer_block-nav_item>
+                                <a class=link href=about.html>Acerca de este Proyecto</a>
+                            </li>
+                            <li class=footer_block-nav_item>
+                                <a class=link href=contacts.html>Contactenos</a>
+                            </li>
+                            <li class=footer_block-nav_item>
+                                <a class=link href=courses.html>Todos los cursos</a>
+                            </li>
+                            <li class=footer_block-nav_item>
+                                <a class=link href=journal.html>Desde el Blog</a>
+                            </li>
+                            <li class=footer_block-nav_item>
+                                <a class=link href=library.html>Biblioteca</a>
+                            </li>
+                            <li class=footer_block-nav_item>
+                                <a class=link href=faq.html>Preguntas mas Frecuentes</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer_block col-sm-6 col-lg-auto m-sm-0" data-order=3>
+                        <h5 class=footer_block-header>Popular Courses:</h5>
+                        <ul class=footer_block-list>
+                            <li class="footer_block-list_item d-flex align-items-baseline">
+                                <span class=marker></span>
+                                <a class=link href=course.html>Conceptos básicos de ciencia de datos CS con Python y SQL</a>
+                            </li>
+                            <li class="footer_block-list_item d-flex align-items-baseline">
+                                <span class=marker></span>
+                                <a class=link href=course.html>Desarrollador Certificado AWS - Asociado 2024
+                                </a>
+                            </li>
+                            <li class="footer_block-list_item d-flex align-items-baseline">
+                                <span class=marker></span>
+                                <a class=link href=course.html>Desarrollador Certificado AWS - Asociado 2020
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer_block col-sm-6 col-lg-auto" data-order=4>
+                        <h5 class=footer_block-header>Instagram:</h5>
+                        <ul class="footer_block-instagram d-grid">
+                            <li class=footer_block-instagram_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <picture>
+                                        <source data-srcset=img/footer/01.webp srcset=img/footer/01.webp>
+                                        <img class=lazy data-src=img/footer/01.jpg src=img/footer/01.jpg alt=media>
+                                    </picture>
+                                </a>
+                            </li>
+                            <li class=footer_block-instagram_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <picture>
+                                        <source data-srcset=img/footer/02.webp srcset=img/footer/02.webp>
+                                        <img class=lazy data-src=img/footer/02.jpg src=img/footer/02.jpg alt=media>
+                                    </picture>
+                                </a>
+                            </li>
+                            <li class=footer_block-instagram_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <picture>
+                                        <source data-srcset=img/footer/03.webp srcset=img/footer/03.webp>
+                                        <img class=lazy data-src=img/footer/03.jpg src=img/footer/03.jpg alt=media>
+                                    </picture>
+                                </a>
+                            </li>
+                            <li class=footer_block-instagram_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <picture>
+                                        <source data-srcset=img/footer/04.webp srcset=img/footer/04.webp>
+                                        <img class=lazy data-src=img/footer/04.jpg src=img/footer/04.jpg alt=media>
+                                    </picture>
+                                </a>
+                            </li>
+                            <li class=footer_block-instagram_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <picture>
+                                        <source data-srcset=img/footer/05.webp srcset=img/footer/05.webp>
+                                        <img class=lazy data-src=img/footer/05.jpg src=img/footer/05.jpg alt=media>
+                                    </picture>
+                                </a>
+                            </li>
+                            <li class=footer_block-instagram_item>
+                                <a class=link href=https://www.instagram.com target=_blank rel="noopener noreferrer">
+                                    <picture>
+                                        <source data-srcset=img/footer/06.webp srcset=img/footer/06.webp>
+                                        <img class=lazy data-src=img/footer/06.jpg src=img/footer/06.jpg alt=media>
+                                    </picture>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class=footer_secondary>
+                <div class="container d-flex flex-column flex-sm-row align-items-center justify-content-sm-between">
+                    <a class=footer_secondary-scroll id=scrollToTop href=#>
+                        <i class="icon-angle-up icon"></i>
+                    </a>
+                    <p class=footer_secondary-copyright>
+                        Copyright @ <span id=currentYear></span>
+                        Raul Ñaupari Araujo
+                    </p>
+                </div>
+            </div>
+        </footer>
---
+++ include/header.php
@@ -0,0 +1,110 @@
+<header class=header data-page=home>
+            <div class="container d-flex flex-wrap justify-content-between align-items-center">
+                <div class="logo header_logo">
+                    <a class="d-inline-flex align-items-center" href=index.html>
+                        <span class=logo_picture>
+                            <img src=svg/logo.svg alt=Edison>
+                        </span>
+                        <span class=text>
+                            <span class=brand>Peru</span>
+                            <span class=text_secondary>Suizo</span>
+                        </span>
+                    </a>
+                </div>
+                <button class=header_trigger type=button data-bs-toggle=collapse data-bs-target=#headerMenu aria-controls=headerMenu>
+                    <span class=line></span>
+                    <span class=line></span>
+                    <span class=line></span>
+                </button>
+                <nav class="header_nav collapse" id=headerMenu>
+                    <ul class=header_nav-list>
+                        <li class=header_nav-list_item>
+                            <a class=nav-item href=index.html data-page=home>Inicio</a>
+                        </li>
+                        <li class="header_nav-list_item dropdown">
+                            <a class="nav-link nav-item dropdown-toggle d-inline-flex align-items-center" href=cours.html data-bs-toggle=collapse data-bs-target=#coursesMenu data-trigger=dropdown aria-expanded=false aria-controls=coursesMenu data-page=courses>
+                                Todos los Cursos <i class="icon-angle-down icon"></i>
+                            </a>
+                            <div class="dropdown-menu collapse" id=coursesMenu>
+                                <ul class=dropdown-list>
+                                    <li class=list-item data-main=true>
+                                        <a class="dropdown-item nav-item" data-page=courses href=courshtml data-main=true>Todos los Cursos</a>
+                                    </li>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=course href=crse.html>Descripcion del curso</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class=header_nav-list_item>
+                            <a class=nav-item href=.html data-page=about>Sobre Nosotros</a>
+                        </li>
+                        <li class=header_nav-list_item>
+                            <a class=nav-item href=.html data-page=pricing>Precios</a>
+                        </li>
+                        <li class="header_nav-list_item dropdown">
+                            <a class="nav-link nav-item dropdown-toggle d-inline-flex align-items-center" href=journal.html data-bs-toggle=collapse data-bs-target=#journalMenu data-trigger=dropdown aria-expanded=false aria-controls=journalMenu data-page=journal>
+                                Diario <i class="icon-angle-down icon"></i>
+                            </a>
+                            <div class="dropdown-menu collapse" id=journalMenu>
+                                <ul class=dropdown-list>
+                                    <li class=list-item data-main=true>
+                                        <a class="dropdown-item nav-item" data-page=journal href=journl.html data-main=true>Diario</a>
+                                    </li>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=post href=pst.html>Publicacion Unica</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="header_nav-list_item dropdown">
+                            <a class="nav-link nav-item dropdown-toggle d-inline-flex align-items-center" href=# data-bs-toggle=collapse data-bs-target=#pagesMenu data-trigger=dropdown aria-expanded=false aria-controls=pagesMenu data-page="">
+                                Paginas <i class="icon-angle-down icon"></i>
+                            </a>
+                            <div class="dropdown-menu collapse" id=pagesMenu>
+                                <!----
+                                <ul class=dropdown-list>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=library href=library.html>Media library</a>
+                                    </li>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=events href=events.html>Events</a>
+                                    </li>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=teachers href=team.html>Teachers</a>
+                                    </li>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=faq href=faq.html>FAQ</a>
+                                    </li>
+                                    <li class=list-item>
+                                        <a class="dropdown-item nav-item" data-page=error href=404.html>404</a>
+                                    </li>
+                                </ul>
+
+                            -->
+                            </div>
+                        </li>
+                        <li class=header_nav-list_item>
+                            <a class=nav-item href=contacts.html data-page=contacts>Contacto</a>
+                        </li>
+                    </ul>
+                    <ul class="promobar_socials d-flex align-items-center justify-content-center">
+                        <li class=promobar_socials-item>
+                            <a class=link href=# target=_blank rel="noopener noreferrer">
+                                <i class=icon-facebook></i>
+                            </a>
+                        </li>
+                        <li class=promobar_socials-item>
+                            <a class=link href=# target=_blank rel="noopener noreferrer">
+                                <i class=icon-twitter></i>
+                            </a>
+                        </li>
+                        <li class=promobar_socials-item>
+                            <a class=link href=# target=_blank rel="noopener noreferrer">
+                                <i class=icon-instagram></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
